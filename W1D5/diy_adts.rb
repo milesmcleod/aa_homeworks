77da8d24c15f0156b4117f443d78fc1af4ee15ee
MilesMcLeod <milesmcleod123@gmail.com>
2017-10-05 23:04:45 -0700

    Build Map

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -33,3 +33,33 @@ class Queue
     @queue.dup
   end
 end
+
+class Map
+  def initialize
+    @map = []
+  end
+
+  def assign(key, value)
+    updated = false
+    @map.each do |el|
+      if el[0] == key
+        el[1] = value
+        updated = true
+      end
+    end
+    @map << [key, value] unless updated
+  end
+
+  def lookup(key)
+    @map.each { |el| return el if el.first == key }
+  end
+
+  def remove(key)
+    @map.each { |el| @map.delete(el) if el.first == key }
+  end
+
+  def show
+    @map.dup
+  end
+
+end
